3330b4428d502dd928c6f5eceb9d47e71f225912
bwearly <153124661+bwearly@users.noreply.github.com>
2025-04-04 13:58:36 -0600

    installed packages and added some security stuff

---
+++ backend/INTEX_2025.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace INTEX_2025.API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
+
+// dotnet ef migrations add "Initial" --context ApplicationDbContext
+// dotnet ef database update --context ApplicationDbContext
