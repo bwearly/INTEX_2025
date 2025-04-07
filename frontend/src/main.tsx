63c67c39ad1d7c60d59d6a375f087f3dac6947f7
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-04-07 15:34:48 -0600

    fixed main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,13 @@
-import * as React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
