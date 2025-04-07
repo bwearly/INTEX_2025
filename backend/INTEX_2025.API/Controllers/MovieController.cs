5472142032231d688224707f28c6fa57fb9d6148
bwearly <153124661+bwearly@users.noreply.github.com>
2025-04-07 11:52:54 -0600

    Created the Application DB and messed around with the genres

--- backend/INTEX_2025.API/Controllers/MovieController.cs
+++ backend/INTEX_2025.API/Controllers/MovieController.cs
@@ -1,6 +1,7 @@
 ﻿using INTEX_2025.API.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace INTEX_2025.API.Controllers
 {
@@ -34,5 +35,25 @@ namespace INTEX_2025.API.Controllers
             
             return Ok(entertainmentType);
         }
+
+        //[HttpGet("MoviesByGenre")]
+        //public IActionResult GetMoviesByGenre()
+        //{
+        //    var genres = new[] { "Action", "Adventure", "Anime Series International TV Shows", "British TV Shows Docuseries International TV Shows", "Children", "Comedies", "Comedies Dramas International Movies", "Comedies International Movies", "Comedies Romantic Movies", "Crime TV Shows Docuseries", "Documentaries", "Domcumentaries International Movies", "Docuseries", "Dramas", "Dramas International Movies", "Dramas Romantic Movies", "Family Movies", "Fantasy", "Horror Movies", "International Movies Thriller", "International TV Shows Romantic TV Shows TV Dramas", "Kids' TV", "Language TV Shows", "Musicals", "Nature TV", "Reality TV", "Spirituality", "TV Action", "TV Comedies", "TV Dramas", "Talk Shows TV Comedies", "Thrillers" };
+        //    var result = new Dictionary<string, List<string>>();
+
+        //    foreach (var genre in genres)
+        //    {
+        //        var movies = _context.MoviesTitles
+        //            .Where(m => EF.Property<int>(m, genre) == 1)
+        //            .Select(m => m.Title)
+        //            .ToList();
+
+        //        result[genre] = movies;
+        //    }
+
+        //    return Ok(result);
+        //}
+
     }
 }
