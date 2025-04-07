de700c319bdb4f4d992e5ad8cb448d642927b463
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-04-07 14:55:00 -0600

    fixing routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,15 @@
-import { useState } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ManageMoviesPage from './pages/admin/ManageMoviesPage';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<></>}></Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/admin/movies" element={<ManageMoviesPage />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
