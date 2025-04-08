7805f1fa7cf7295ab7649280768bb9b03e8a3805
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-04-07 20:49:45 -0600

    crud working (maybe)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Home from './pages/Home/Home';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import Curtain from './components/common/Curtain';
+
 const root = createRoot(document.getElementById('root')!);
+
 root.render(
   <StrictMode>
-    <Curtain>
-      <Home />
-    </Curtain>
+    <BrowserRouter>
+      <Curtain>
+        <App />
+      </Curtain>
+    </BrowserRouter>
   </StrictMode>
 );
