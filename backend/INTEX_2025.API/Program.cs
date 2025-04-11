8aa6981ca6c6522e6c416209c542349a22f99b8a
Your Name <your-email@example.com>
2025-04-10 21:40:25 -0600

    changed connection string

--- backend/INTEX_2025.API/Program.cs
+++ backend/INTEX_2025.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSwaggerGen();
 
 // Add databases
 builder.Services.AddDbContext<MoviesDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CompetitionConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieConnection")));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("IdentityConnection")));
