6a2298c47e08da416414b3d53af5159add6c84c2
Your Name <your-email@example.com>
2025-04-10 21:21:36 -0600

    deployed movies database

--- backend/INTEX_2025.API/Program.cs
+++ backend/INTEX_2025.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSwaggerGen();
 
 // Add databases
 builder.Services.AddDbContext<MoviesDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("CompetitionConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CompetitionConnection")));
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("IdentityConnection")));
